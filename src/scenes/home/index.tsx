2acacbfe67e1c471897505eb99b7bd9f2e298b51
Good_Cthulhu <hayyabusa_01@mail.ru>
2023-04-03 07:59:18 +0300

    fix

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -20,7 +20,8 @@ const Home = ({ setSelectedPage }: Props) => {
   return (
     <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
       {/* Image and main header */}
-      <motion.div className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6 "
+      <motion.div
+        className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6 "
         onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
       >
         {/* Main Header */}
@@ -49,17 +50,17 @@ const Home = ({ setSelectedPage }: Props) => {
             </p>
           </motion.div>
           {/* Actions */}
-          <motion.div className=" mt-8 flex  items-center gap-2"
+          <motion.div
+            className=" mt-8 flex  items-center gap-2"
             initial="hidden"
             whileInView="visible"
             viewport={{ once: true, amount: 0.5 }}
-            transition={{ delay: 0.2 ,duration: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
             variants={{
               hidden: { opacity: 0, x: -50 },
               visible: { opacity: 1, x: 0 },
             }}
           >
-            
             <ActionButton setSelectedPage={setSelectedPage}>
               Join Now
             </ActionButton>
